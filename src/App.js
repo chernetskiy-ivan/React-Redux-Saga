1bc939d8f2af869c0fde1af0b861375cf1be3d5a
Ivan <ivanchernetskey@mail.ru>
2021-03-18 20:38:30 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <div className="container">
-
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+            FORM
+        </div>
+      </div>
+        <div className="row">
+            <div className="col">
+                POSTS
+            </div>
+            <div className="col">
+                FETCH POSTS
+            </div>
+        </div>
     </div>
   );
 }
