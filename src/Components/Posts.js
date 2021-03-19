acab7050a2ce7d130b8d172e6d716a72995db415
Ivan <ivanchernetskey@mail.ru>
2021-03-19 16:56:40 +0300

    chek in posts is post survive

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,5 +2,8 @@ import React from 'react'
 import Post from './Post'
 
 export default function Posts({posts}) {
+    if(!posts.length) {
+        return <p>Постов пока нет</p>
+    }
     return posts.map(post => <Post post={post} key={post} />)
 }
