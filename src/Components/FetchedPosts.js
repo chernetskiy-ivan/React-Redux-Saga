60882fdc67e0da46db87fbc73eb784f02ce92aa9
Ivan <ivanchernetskey@mail.ru>
2021-03-19 17:01:46 +0300

    chek post in FetchedPosts

--- src/Components/FetchedPosts.js
+++ src/Components/FetchedPosts.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import Post from "./Post";
 
-export default () => {
-    return (
-        <div>
-            <h1>Fetched Posts</h1>
-        </div>
-    )
+export default ({posts}) => {
+    if(!posts.length) {
+        return <button className='btn btn-primary'>Загрузить</button>
+    }
+    return posts.map(post => <Post post={post} key={post} />)
 }
