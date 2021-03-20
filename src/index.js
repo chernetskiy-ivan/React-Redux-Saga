b3661e5c8fa101b7c129cf8219bcb9a9460ac129
Ivan <ivanchernetskey@mail.ru>
2021-03-20 16:42:26 +0300

    linked React with Redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import {rootReducer} from "./redux/rootReducers";
 
 const store = createStore(rootReducer)
 
-render(<App />, document.getElementById('root'));
+//Компонент Provider - компонент высшего порядка
+//Не создает шаблон, а добавляет функциональность к компоненту App
+//В данно случае сы связали React с redux
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
