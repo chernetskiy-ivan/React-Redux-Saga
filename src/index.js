b9f478836f26176d85d0b261eba4ebc96f88b84a
Ivan <ivanchernetskey@mail.ru>
2021-03-20 16:34:43 +0300

    created rootReducers file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+
+const store = createStore()
 
 render(<App />, document.getElementById('root'));
 
