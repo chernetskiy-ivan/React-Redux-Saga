4a556f98156b2ba97773db82e4508be0d877c8f2
Ivan <ivanchernetskey@mail.ru>
2021-03-22 21:19:37 +0300

    we changed fetchPosts for check problems in this action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -44,12 +44,18 @@ export function hideAlert() {
 
 export function fetchPosts() {
     return async dispatch => {
-        dispatch(showLoader())
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        const json = await response.json()
-        setTimeout(() => {
-            dispatch({type: FETCH_POSTS, payload: json})
+        try {
+            dispatch(showLoader())
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+            const json = await response.json()
+            setTimeout(() => {
+                dispatch({type: FETCH_POSTS, payload: json})
+                dispatch(hideLoader())
+            }, 500)
+        } catch(e) {
+            dispatch(showAlert('Что-то пошло не так'))
             dispatch(hideLoader())
-        }, 500)
+        }
+
     }
 }
