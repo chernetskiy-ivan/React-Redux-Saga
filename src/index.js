5536dc477685c57a2326da3501f35078665c5998
Ivan <ivanchernetskey@mail.ru>
2021-03-24 11:20:36 +0300

    created generator in sagas, import createSagaMiddleware and use method run add sagaWatcher to saga(created by createSagaMiddleware() )

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux'
 import {compose, createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {rootReducer} from "./redux/rootReducers";
 import {forbiddenWordsMiddleware} from "./redux/middleware";
+import {sagaWatcher} from "./redux/sagas";
+
+//создам объект саги
+const saga = createSagaMiddleware()
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk, forbiddenWordsMiddleware),
+    applyMiddleware(thunk, forbiddenWordsMiddleware, saga),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+//привязываем вотчер к саге с помощью метода run
+saga.run(sagaWatcher)
 
 //Компонент Provider - компонент высшего порядка
 //Не создает шаблон, а добавляет функциональность к компоненту App
